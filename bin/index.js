c4707229fc16aa45b68826ebfc7559b94b56c551
Matt Jennings <mattj52990@gmail.com>
2021-10-08 10:53:16 -0400

    Handle errors

--- bin/index.js
+++ bin/index.js
@@ -6,15 +6,20 @@ const argv = require("minimist")(process.argv.slice(2));
 
 const cmdMap = { build, lint };
 
-if (cmdMap[argv._]) {
-  cmdMap[argv._]({
-    inputPath: argv.i,
-    outputPath: argv.o,
-    contexts: argv.contexts,
-    ...JSON.parse(
-      fs.readFileSync(path.resolve(argv.config), { encoding: "utf-8" })
-    ),
-  });
-} else {
-  throw new Error("Invalid command supplied");
+try {
+  if (cmdMap[argv._]) {
+    cmdMap[argv._]({
+      inputPath: argv.i,
+      outputPath: argv.o,
+      contexts: argv.contexts,
+      ...JSON.parse(
+        fs.readFileSync(path.resolve(argv.config), { encoding: "utf-8" })
+      ),
+    });
+  } else {
+    throw new Error("Invalid command supplied");
+  }
+} catch (e) {
+  console.error(e);
+  process.exit(1);
 }
